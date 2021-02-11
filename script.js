444387154e6651e3654af314018bcb69813569dd
bezo16 <bezo16@protonmail.ch>
2021-02-11 17:09:06 +0100

    pridane zvuky na čierne (no racist) klavesy

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-var notesArray = ['c2','d2','e2','f2','g2','b2','c3','d3','e3','f3','g3','b3','c4','d4','e4','f4','g4','b4','c5','d5','e5','f5','g5','b5',]
-var docasnarray = ['c2','d2']
-var c2='c2'
+
+
 
 var c2 = new Audio("piano/448547__tedagame__c2.ogg")
 var d2 = new Audio("piano/448607__tedagame__d2.ogg")
@@ -34,106 +33,62 @@ var g5 = new Audio("piano/448553__tedagame__g5.ogg")
 var a5 = new Audio("piano/448560__tedagame__a5.ogg")
 var b5 = new Audio("piano/448537__tedagame__b5.ogg")
 
+var c_2 = new Audio("piano/448541__tedagame__c-2.ogg")
+var d_2 = new Audio("piano/448600__tedagame__d-2.ogg")
+var f_2 = new Audio("piano/448587__tedagame__f-2.ogg")
+var g_2 = new Audio("piano/448590__tedagame__g-2.ogg")
+var a_2 = new Audio("piano/448571__tedagame__a-2.ogg")
+ 
+var c_3 = new Audio("piano/448538__tedagame__c-3.ogg")
+var d_3 = new Audio("piano/448601__tedagame__d-3.ogg")
+var f_3 = new Audio("piano/448584__tedagame__f-3.ogg")
+var g_3 = new Audio("piano/448593__tedagame__g-3.ogg")
+var a_3 = new Audio("piano/448570__tedagame__a-3.ogg")
 
+var c_4 = new Audio("piano/448539__tedagame__c-4.ogg")
+var d_4 = new Audio("piano/448602__tedagame__d-4.ogg")
+var f_4 = new Audio("piano/448585__tedagame__f-4.ogg")
+var g_4 = new Audio("piano/448592__tedagame__g-4.ogg")
+var a_4 = new Audio("piano/448577__tedagame__a-4.ogg")
+
+var c_5 = new Audio("piano/448532__tedagame__c-5.ogg")
+var d_5 = new Audio("piano/448603__tedagame__d-5.ogg")
+var f_5 = new Audio("piano/448582__tedagame__f-5.ogg")
+var g_5 = new Audio("piano/448599__tedagame__g-5.ogg")
+var a_5 = new Audio("piano/448576__tedagame__a-5.ogg")
+
+var notesArray = [c2,d2,e2,f2,g2,a2,b2,c3,d3,e3,f3,g3,a3,b3,c4,d4,e4,f4,g4,a4,b4,c5,d5,e5,f5,g5,a5,b5,]
+var notesArrayBlack = [c_2,d_2,f_2,g_2,a_2,c_3,d_3,f_3,g_3,a_3,c_4,d_4,f_4,g_4,a_4,c_5,d_5,f_5,g_5,a_5]
+
+var x = 0
+var y = 0
 // funkcie
 
+notesArray.forEach(el => {
+    document.querySelectorAll('.key')[x].addEventListener('click',() =>{
+        playAudio(el)
+        
+    })
+    x++
+});
+notesArrayBlack.forEach(el => {
+    document.querySelectorAll('.blackkey')[y].addEventListener('click',(e) =>{
+        playAudio(el)
+        e.stopPropagation()
+    })
+    y++
+});
+
 var playAudio = (note) => {
+    
     note.currentTime=0;
     note.play()
     }
 
 
  
+   
 
-
-
-$(document).ready(() => { /// WHITEKEYS
-    $(".whitekey1").click(() =>{
-        playAudio(c2)
-            })
-    $(".whitekey2").click(() =>{
-        playAudio(d2)
-            })  
-    $(".whitekey3").click(() =>{
-        playAudio(e2)
-            })
-    $(".whitekey4").click(() =>{
-        playAudio(f2)
-            })   
-    $(".whitekey5").click(() =>{
-        playAudio(g2)
-            })
-    $(".whitekey6").click(() =>{
-        playAudio(a2)
-            })  
-    $(".whitekey7").click(() =>{
-        playAudio(b2)
-            })  
-    $(".whitekey8").click(() =>{
-        playAudio(c3)
-            })
-    $(".whitekey9").click(() =>{
-        playAudio(d3)
-            })  
-    $(".whitekey10").click(() =>{
-        playAudio(e3)
-            })
-    $(".whitekey11").click(() =>{
-        playAudio(f3)
-            })   
-    $(".whitekey12").click(() =>{
-        playAudio(g3)
-            })
-    $(".whitekey13").click(() =>{
-        playAudio(a3)
-            })  
-    $(".whitekey14").click(() =>{
-        playAudio(b3)
-            })  
-    $(".whitekey15").click(() =>{
-        playAudio(c4)
-            })
-    $(".whitekey16").click(() =>{
-        playAudio(d4)
-            })  
-    $(".whitekey17").click(() =>{
-        playAudio(e4)
-            })
-    $(".whitekey18").click(() =>{
-        playAudio(f4)
-            })   
-    $(".whitekey19").click(() =>{
-        playAudio(g4)
-            })
-    $(".whitekey20").click(() =>{
-        playAudio(a4)
-            })  
-    $(".whitekey21").click(() =>{
-        playAudio(b4)
-            }) 
-    $(".whitekey22").click(() =>{
-        playAudio(c5)
-            })
-    $(".whitekey23").click(() =>{
-        playAudio(d5)
-            })  
-    $(".whitekey24").click(() =>{
-        playAudio(e5)
-            })
-    $(".whitekey25").click(() =>{
-        playAudio(f5)
-            })   
-    $(".whitekey26").click(() =>{
-        playAudio(g5)
-            })
-    $(".whitekey27").click(() =>{
-        playAudio(a5)
-            })  
-    $(".whitekey28").click(() =>{
-        playAudio(b5)
-            })
-                
-})
 
     addEventListener('keyup',(e) => {
         if(e.code === 'KeyQ')playAudio(c2)
@@ -164,6 +119,27 @@ $(document).ready(() => { /// WHITEKEYS
         if(e.code === 'KeyM')playAudio(g5)
         if(e.code === 'Comma')playAudio(a5)
         if(e.code === 'Period')playAudio(b5)
+
+        if(e.code === 'Backquote')playAudio(c_2)
+        if(e.code === 'Digit1')playAudio(d_2)
+        if(e.code === 'Digit2')playAudio(f_2)
+        if(e.code === 'Digit3')playAudio(g_2)
+        if(e.code === 'Digit4')playAudio(a_2)
+        if(e.code === 'Digit5')playAudio(c_3)
+        if(e.code === 'Digit6')playAudio(d_3)
+        if(e.code === 'Digit7')playAudio(f_3)
+        if(e.code === 'Digit8')playAudio(g_3)
+        if(e.code === 'Digit9')playAudio(a_3)
+        if(e.code === 'Digit0')playAudio(c_4)
+        if(e.code === 'Minus')playAudio(d_4)
+        if(e.code === 'Equal')playAudio(f_4)
+        if(e.code === 'BracketRight')playAudio(g_4)
+        if(e.code === 'BracketLeft')playAudio(a_4)
+        if(e.code === 'Semicolon')playAudio(c_5)
+        if(e.code === 'Quote')playAudio(d_5)
+        if(e.code === 'Backslash')playAudio(f_5)
+        if(e.code === 'NumpadDivide')playAudio(g_5)
+        if(e.code === 'NumpadMultiply')playAudio(a_5)
         console.log(e.code)
     })
     
